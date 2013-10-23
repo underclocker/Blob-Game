16cc6a3b29e094503ed79ce5013693acbca636fb
underclocker <underclocker@gmail.com>
2013-10-23 17:22:41 -0400

    charselect and earth update

--- BlobGame/src/org/siggd/actor/Earth.java
+++ BlobGame/src/org/siggd/actor/Earth.java
@@ -46,8 +46,8 @@ public class Earth extends Actor implements IObserver {
 		Vector2 origin = new Vector2();
 		mBody = makeBody(mName, 4096, BodyType.KinematicBody, origin, false);
 		mDrawable.mDrawables.add(new BodySprite(mBody, origin, mTex));
-		setProp("Friction", (Float) .5f);
-		mBody.setAngularVelocity(.23f);
+		setProp("Friction", (Float) .4f);
+		mBody.setAngularVelocity(.21f);
 	}
 
 	public boolean inputActive() {
